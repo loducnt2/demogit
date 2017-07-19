c551cb0681539a0acd5751632412c41f075e0bc5
DucLT <loducnt2@gmail.com>
2017-07-19 20:33:08 +0700

    jdk

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Category;
+use Session;
 class CategoryController extends Controller
 {
     /**
